b22cf64db4f76b8e76afc9ff59728ceccb732ebc
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2024-12-02 20:11:40 -0300

    memory game

---
+++ script.js
@@ -0,0 +1,74 @@
+const emojis = [
+    "💻",
+    "💻",
+    "💿",
+    "💿",
+    "🤖",
+    "🤖",
+    "👨‍💻",
+    "👨‍💻",
+    "🎧",
+    "🎧",
+    "⚙️",
+    "⚙️",
+    "🚄",
+    "🚄",
+    "🏍️",
+    "🏍️",
+    "🚲",
+    "🚲",
+    "⚓",
+    "⚓",
+    "🛟",
+    "🛟",
+    "☠️",
+    "☠️",
+    "🌍",
+    "🌍",
+    "🪶",
+    "🪶",
+
+
+]
+let openCards = []
+let shuffleEmojis = emojis.sort(() =>(Math.random() > 0.5 ? 2 : -1));
+
+for(let i = 0; i < emojis.length; i++)
+{
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+
+function handleClick() {
+    if(openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);f
+    }
+
+    if(openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch() {
+    if(openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length) {
+        alert("Voce venceu:")
+    }
+
+
+}
+
